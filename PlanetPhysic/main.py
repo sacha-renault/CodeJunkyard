5d39e4b311e190a9f867406ea4b5da313a8cdff6
Sacha RENAULT <sacha35340@gmail.com>
2024-11-18 13:55:40 +0100

    feat: use a json to initialize the system

--- PlanetPhysic/main.py
+++ PlanetPhysic/main.py
@@ -1,18 +1,38 @@
 import random
+import os
+import json
 from src.physic import create_body, Coordinates
 from src.physic.engine import System
 from src.ui import Ui
 
 
 if __name__ == "__main__":
-    engine = System(0.01)
-    renderer = Ui(engine , screen_size = (800, 600))
-    for _ in range(50):
-        engine.add_body(
+    # get path of this folder
+    path = os.path.abspath(os.path.dirname(__file__))
+
+    # create a system (engine)
+    system = System(0.0001)
+
+    # create the object that will render
+    renderer = Ui(system , screen_size = (800, 600), step_per_render=100, scale=2)
+
+    # open the init.json
+    with open(os.path.join(path, "init.json")) as file:
+        objects: list[dict] = json.load(file)
+
+    for obj in objects:
+        x = obj.get("x", renderer.screen_size[0] // 2)
+        y = obj.get("y", renderer.screen_size[1] // 2)
+        vx = obj.get("vx", 0)
+        vy = obj.get("vy", 0)
+        radius = obj.get("radius", 10)
+        density = obj.get("density", 1)
+
+        system.add_body(
             create_body(renderer.DIMENSION,
-                        random.random() * 10,
-                        random.random() * 10,
-                        Coordinates.random(2, [(0, 800), (0, 600)]),
-                        Coordinates.random(2, (-100, 100))
+                        radius,
+                        density,
+                        Coordinates([x, y]),
+                        Coordinates([vx, vy])
         ))
     renderer.run()
