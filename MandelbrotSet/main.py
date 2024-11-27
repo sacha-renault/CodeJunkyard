fix: optimize mandelbrot computation

MandelbrotSet/main.py
@@ -1,20 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from src import initialize_grid, mandelbrot, fc
+from src import display_mandelbrot
 
 if __name__ == "__main__":
     # parameters
     overflow_limit = 1e5
-    max_iter = 500
-    grid_size = 500
+    max_iter = 100
+    grid_size = 750, 750
     aabb = (-2, 1, -1.5, 1.5)
+    display_mandelbrot(grid_size, aabb, max_iter, overflow_limit)
 
-    # computation
-    grid = initialize_grid((grid_size, grid_size), *aabb)
-    mask = mandelbrot(grid, max_iter, overflow_limit, fc)
 
-    # display
-    plt.imshow(mask / np.max(mask), extent=aabb, cmap='hot')
-    plt.colorbar()
-    plt.title("Mandelbrot Set")
-    plt.show()

MandelbrotSet/src/__init__.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Callable, Optional
+import matplotlib.pyplot as plt
 import numpy as np
 
 complexArray = np.ndarray[np.complex128]
@@ -9,6 +10,31 @@ def initialize_grid(dimensions: Tuple[int, int],
                     real_max: float,
                     complexe_min: Optional[float] = None,
                     complexe_max: Optional[float] = None) -> complexArray:
+    """
+    Initializes a 2D grid of complex numbers for Mandelbrot set computation.
+
+    The grid spans the specified ranges for the real and imaginary parts of the complex numbers.
+    If `complexe_min` or `complexe_max` are not provided, they default to `real_min` and `real_max`, respectively.
+
+    Args:
+        dimensions (Tuple[int, int]):
+            The dimensions of the grid as (rows, columns). The number of rows corresponds
+            to the resolution along the imaginary axis, and the number of columns corresponds
+            to the resolution along the real axis.
+        real_min (float):
+            The minimum value of the real part of the complex numbers.
+        real_max (float):
+            The maximum value of the real part of the complex numbers.
+        complexe_min (Optional[float], optional):
+            The minimum value of the imaginary part of the complex numbers. Defaults to `real_min` if not provided.
+        complexe_max (Optional[float], optional):
+            The maximum value of the imaginary part of the complex numbers. Defaults to `real_max` if not provided.
+
+    Returns:
+        np.ndarray:
+            A 2D array of complex numbers representing the grid. Each element is a complex number
+            formed by combining the real and imaginary parts corresponding to the grid's position.
+    """
     # set up limit for complex
     complexe_min = complexe_min if complexe_min is not None else real_min
     complexe_max = complexe_max if complexe_max is not None else real_max
@@ -25,15 +51,51 @@ def initialize_grid(dimensions: Tuple[int, int],
     return grid
 
 def fc(z: complexArray, c: complexArray) -> complexArray:
+    """basic mandelbrot function `fc`"""
     return z ** 2 + c
 
 def mandelbrot(c: complexArray, max_iter: int, overflow_limit: float, fc: Callable[[complexArray], complexArray]) -> maskArray:
+    # z starts as 0
     z = np.zeros(c.shape, dtype=np.complex128)
+
+    # init an empty mask
     mask = np.zeros(c.shape, dtype=int)
+
+    # loop to do all iterations
     for i in range(max_iter):
-        z = fc(z, c)
+
+        # calculate where current mask is 0
+        null_mask = (mask == 0)
+
+        # compute next z with provided function
+        # compute only values that are non 0
+        z[null_mask] = fc(z[null_mask], c[null_mask])
+
+        # get a mask of overflowing values
         overflow = np.abs(z) > overflow_limit
-        mask[overflow & (mask == 0)] = i
+
+        # all the value in the mask will be change to the step number
+        mask[overflow & null_mask] = i
+
+        # values that overflowed are changed to 0 (no need to recalculate)
         z[overflow] = 0
+
+    # last calculation of where mask is 0
     mask[mask == 0] = max_iter
+
     return mask
+
+def display_mandelbrot(grid_size: Tuple[int, int],
+                       aabb: Tuple[float, float, float, float],
+                       max_iter: int,
+                       overflow_limit: float) -> None:
+    # computation
+    grid = initialize_grid(grid_size, *aabb)
+    mask = mandelbrot(grid, max_iter, overflow_limit, lambda z, c : z**2 + c)
+
+    # display
+    fig, ax = plt.subplots(1, 1, figsize=(10,10))
+    ax.imshow(mask / np.max(mask), extent=aabb, cmap='hot')
+    fig.tight_layout()
+    plt.title("Mandelbrot Set")
+    plt.show()